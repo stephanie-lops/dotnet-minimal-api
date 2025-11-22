2ff1b00cdb355864bb1bd0f82dfba889511fe9da
stephanie-lops <stephanielopees@gmail.com>
2025-11-21 23:04:17 -0400

    iniciando projeto

---
+++ Program.cs
@@ -0,0 +1,15 @@
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello Minimal API!");
+
+app.MapPost("/login", (MinimalApi.DTOs.LoginDTO loginDTO) => {
+    if(loginDTO.Email == "adm@teste.com" && loginDTO.Senha == "123456")
+        return Results.Ok("Login com sucesso");
+    
+    else
+        return Results.Unauthorized();   
+});
+
+app.Run();
+
