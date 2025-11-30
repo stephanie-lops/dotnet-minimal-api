d6af916428a9860608ed6527e9ca0596957d67d4
stephanie-lops <stephanielopees@gmail.com>
2025-11-30 13:42:15 -0400

    GET para 1 veiculo

--- Program.cs
+++ Program.cs
@@ -64,6 +64,16 @@ app.MapGet("/veiculos", ([FromQuery] int? pagina, IVeiculoServico veiculoServico
 
     return Results.Ok(veiculos);
 }).WithTags("Veiculos");
+
+app.MapGet("/veiculos/{id}", ([FromRoute] int id, IVeiculoServico veiculoServico) => {
+    
+    var veiculo = veiculoServico.BuscaPorId(id);
+
+    if(veiculo == null)
+        return Results.NotFound();
+
+    return Results.Ok(veiculo);
+}).WithTags("Veiculos");
 #endregion
 
 #region App
