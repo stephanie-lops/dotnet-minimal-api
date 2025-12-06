61ac801f08ad539e6e40d7ab1cef4b1bb3463834
stephanie-lops <stephanielopees@gmail.com>
2025-12-05 20:54:18 -0400

    end points dos adms

--- Dominio/DTOs/AdministradorDTO.cs
+++ Dominio/DTOs/AdministradorDTO.cs
@@ -4,6 +4,7 @@ using MinimalApi.Dominio.Enuns;
 namespace MinimalApi.DTOs;
 public class AdministradorDTO
 {
+    public int Id { get; set; } = default!;
     public string Email { get; set; } = default!;
     public string Senha { get; set; } = default!;
     public Perfil? Perfil { get; set; } = default!;
--- Program.cs
+++ Program.cs
@@ -9,7 +9,7 @@ using System.IO.Pipes;
 using MinimalApi.Dominio.Entidades;
 using MinimalApi.Dominio.Enuns;
 
-// Video "Criando endpoints para adm, 16:00" 
+// Video "Configuranco token JWT no projeto" 
 
 #region Buider
 var builder = WebApplication.CreateBuilder(args);
@@ -46,14 +46,30 @@ app.MapPost("/administradores/login", ([FromBody] LoginDTO loginDTO, IAdministra
 }).WithTags("Administradores");
 
 app.MapGet("/administradores", ([FromQuery] int? pagina, IAdministradorServico administradorServico) => {
-        return Results.Ok(administradorServico.Todos(pagina));
+    var adms =  new List<AdministradorModelView>();
+    var administradores = administradorServico.Todos(pagina);
+    foreach(var adm in administradores)
+    {
+        adms.Add(new AdministradorModelView
+        {
+            Id = adm.Id,
+            Email = adm.Email,
+            Perfil = adm.Perfil
+        });
+    }
+        return Results.Ok(adms);
 }).WithTags("Administradores");
 
 app.MapGet("/administradores/{id}", ([FromRoute] int id, IAdministradorServico administradorServico) => {
     var administrador = administradorServico.BuscaPorId(id);
     if(administrador == null)
         return Results.NotFound();
-    return Results.Ok(administrador);
+    return Results.Ok(new AdministradorModelView
+        {
+            Id = administrador.Id,
+            Email = administrador.Email,
+            Perfil = administrador.Perfil
+        });
 }).WithTags("Administradores");
 
 app.MapPost("/administradores", ([FromBody] AdministradorDTO administradorDTO, IAdministradorServico administradorServico) => {
@@ -72,15 +88,20 @@ app.MapPost("/administradores", ([FromBody] AdministradorDTO administradorDTO, I
     if(validacao.Mensagens.Count > 0)
         return Results.BadRequest(validacao);
 
-    var veiculo = new Administrador{
+    var administrador = new Administrador{
         Email = administradorDTO.Email,
         Senha = administradorDTO.Senha,
-        Perfil = administradorDTO.Perfil.ToString() ?? Perfil.editor.ToString()
+        Perfil = administradorDTO.Perfil.ToString() ?? Perfil.Editor.ToString()
     };
     
-    administradorServico.Incluir(veiculo);
+    administradorServico.Incluir(administrador);
 
-    return Results.Created($"/administrador/{veiculo.Id}", veiculo);
+    return Results.Created($"/administrador/{administrador.Id}", new AdministradorModelView
+        {
+            Id = administrador.Id,
+            Email = administrador.Email,
+            Perfil = administrador.Perfil
+        });;
 }).WithTags("Administradores");
 #endregion
 
